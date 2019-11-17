8f04489135e036f57c8b4d2056cbf06ed2050177
Kevin McCarthy <km.mccarthy11@gmail.com>
2019-11-17 16:46:30 -0500

    inital database creation (accounts & profile)

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,32 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS healthApp_db;
+CREATE DATABASE healthApp_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE TABLE accounts (
+
+    id INT (11) AUTO_INCREMENT NOT NULL,
+    user VARCHAR(255) NOT NULL,
+    pass VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+
+);
+
+CREATE TABLE profiles (
+
+    id INT (11) AUTO_INCREMENT NOT NULL,
+    nameFirst VARCHAR(255) NOT NULL,
+    nameLast VARCHAR(255) NOT NULL,
+    age INT (11) NOT NULL,
+    height INT (11) NOT NULL,
+    bodymass INT (11) NOT NULL,
+    allergies VARCHAR (2000) NOT NULL,
+    medication VARCHAR (2000) NOT NULL,
+    specialNeeds VARCHAR (5000) NOT NULL,
+    PRIMARY KEY (id)
+
+);
+
+
+
+
+
+
