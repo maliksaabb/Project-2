63198213d14b5607749d27134274cf63df4ef53d
Kevin McCarthy <km.mccarthy11@gmail.com>
2019-11-19 18:16:57 -0500

    tweaks to sql schema

--- models/schema.sql
+++ models/schema.sql
@@ -19,7 +19,19 @@ CREATE TABLE profiles (
     height INT (11) NOT NULL,
     bodymass INT (11) NOT NULL,
     allergies VARCHAR (2000) NOT NULL,
-    medication VARCHAR (2000) NOT NULL,
+    medication1 VARCHAR (250),
+    dosage1 INT (11),
+    timesperday1 INT (11),
+    medication2 VARCHAR (250),
+    dosage2 INT (11),
+    timesperday2 INT (11),
+    medication3 VARCHAR (250),
+    dosage3 INT (11),
+    timesperday3 INT (11),
+    medication4 VARCHAR (250),
+    dosage4 INT (11),
+    timesperday4 INT (11),
+
     specialNeeds VARCHAR (5000) NOT NULL,
     PRIMARY KEY (id)
 
