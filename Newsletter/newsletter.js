1728f2686930b5ebe88c8f341928b975931da9c9
Anne Palangi <anne.palangi@gmail.com>
2019-11-21 21:05:34 -0500

    newsletter crap

---
+++ Newsletter/newsletter.js
@@ -0,0 +1,23 @@
+
+    // queryURL is the url we'll use to query the API
+    var queryURL = "https://api.nytimes.com/svc/news/v3/content/all/health.json?api-key=voB5tFrjVke8lhzEShfx6GKOO0kfJUHS";
+    
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    .then(function(response){
+        var health = response;
+
+      $("#healthFeed").html(health);
+      for (var i = 0; i < 3; i++) {
+        response.results[i];
+        $("#healthFeed").append("<p>" + response.results[i].title + "</p>");
+        $("#healthFeed").append("<p>" + response.results[i].url + "</p>");
+        console.log(response.results[i]);
+      }
+  
+    })
+     
+    
+
