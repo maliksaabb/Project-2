d1c91297306cd3aa1ffa27abbdd351a71d7ba138
Kevin McCarthy <km.mccarthy11@gmail.com>
2019-11-21 20:22:12 -0500

    moved HTML pages to views, edited sign in/log in to work with passport

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,40 @@ var $exampleText = $("#example-text");
 var $exampleDescription = $("#example-description");
 var $submitBtn = $("#submit");
 var $exampleList = $("#example-list");
+//-------------------------------------------------------------------
+//START imported from sequelize index.js template
+const config = require(__dirname + "/../config/config.json")[env];
+const db = {};
 
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
+//--------------------------------------------------------------------------------------------------------------
+//END imported from sequelize index.js template
+/*
 // The API object contains methods for each kind of request we'll make
 var API = {
   saveExample: function(example) {
@@ -97,3 +130,4 @@ var handleDeleteBtnClick = function() {
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick);
+*/
