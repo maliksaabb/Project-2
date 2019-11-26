c8197fadb52656a79a3b2b3d8851937c8e414bbc
Kevin McCarthy <km.mccarthy11@gmail.com>
2019-11-26 09:51:21 -0500

    tweaked dashboard/form/formjs

--- public/js/form.js
+++ public/js/form.js
@@ -2,8 +2,7 @@ $(document).ready(function() {
     // Gets an optional query string from our url (i.e. ?post_id=23)
     var url = window.location.search;
     var postId;
-    // Sets a flag for whether or not we're updating a post to be false initially
-    var updating = false;
+    var app = express();
   
     // If we have this section in our url, we pull out the post id from the url
     // In localhost:8080/cms?post_id=1, postId is 1
